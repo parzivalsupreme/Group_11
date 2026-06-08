8319ce4ee8b8aaa44ad04acf83fa2d306130fd79
CpDanduan <danduanpaul51@gmail.com>
2026-06-08 22:51:05 +0800

    Way too lazy to go through everything and modify

--- src/group11_Project/data/AppData.java
+++ src/group11_Project/data/AppData.java
@@ -6,12 +6,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppData {
+	private String currentUser; // New
+	private String currentRole; // New
+	
     private final List<LedgerEntry> ledger = new ArrayList<>();
     private final List<String[]> users = new ArrayList<>();
 
+    public String getCurrentUser() { // New (Start here)
+	    return currentUser;
+	}
+
+	public void setCurrentUser(String currentUser) {
+	    this.currentUser = currentUser;
+	}
+
+	public String getCurrentRole() {
+	    return currentRole;
+	}
+
+	public void setCurrentRole(String currentRole) {
+	    this.currentRole = currentRole;
+	} // End here
+    
     public AppData() {
-        users.add(new String[]{"Admin User", "admin", "Admin", "Full Access"});
-        Database.loadLedger(ledger); // For the Database
+    	Database.loadUsers(users); // New (Start here)
+        Database.loadLedger(ledger);
+
+        if(users.isEmpty()) {
+
+            users.add(new String[]{
+                "Administrator",
+                "admin",
+                "admin123",
+                "Admin",
+                "Full Access"
+            });
+
+            Database.saveUsers(users);
+        } // End here
     }
 
     public List<LedgerEntry> getLedger() { return ledger; }
--- src/group11_Project/data/Database.java
+++ src/group11_Project/data/Database.java
@@ -1,6 +1,8 @@
 package group11_Project.data;
 
 import group11_Project.model.LedgerEntry;
+import java.io.File; // New
+import java.io.IOException; // New
 
 import java.io.*;
 import java.time.LocalDateTime;
@@ -9,6 +11,7 @@ import java.util.List;
 public class Database {
 
 	private static final String LEDGER_FILE = "ledger.txt";
+	private static final String USERS_FILE = "users.txt"; // New
 
     // Save all ledger entries
     public static void saveLedger(List<LedgerEntry> ledger) {
@@ -33,6 +36,72 @@ public class Database {
             e.printStackTrace();
         }
     }
+    
+    private static void ensureFileExists(String fileName) { // New (Start Here)
+
+        try {
+
+            File file = new File(fileName);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // end here
+    
+    // New (Start here)
+    public static void saveUsers(List<String[]> users) {
+
+        ensureFileExists(USERS_FILE);
+
+        try(PrintWriter pw =
+            new PrintWriter(
+                new FileWriter(USERS_FILE))) {
+
+            for(String[] user : users) {
+
+                pw.println(
+                    user[0] + "|" +
+                    user[1] + "|" +
+                    user[2] + "|" +
+                    user[3]
+                );
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    } // End here
+    
+    // New (Start here)
+    public static void loadUsers(List<String[]> users) {
+
+        users.clear();
+
+        ensureFileExists(USERS_FILE);
+
+        try(BufferedReader br =
+            new BufferedReader(
+                new FileReader(USERS_FILE))) {
+
+            String line;
+
+            while((line = br.readLine()) != null) {
+
+                String[] data = line.split("\\|");
+
+                if(data.length == 4) {
+                    users.add(data);
+                }
+            }
+
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    } // New
 
     // Load all ledger entries
     public static void loadLedger(List<LedgerEntry> ledger) {
