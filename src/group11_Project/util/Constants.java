8c826adf88550fef23ba8e5dea94dbb6603a8fb7
CpDanduan <danduanpaul51@gmail.com>
2026-06-14 13:52:23 +0800

    just added extra years

--- src/group11_Project/util/Constants.java
+++ src/group11_Project/util/Constants.java
@@ -9,7 +9,11 @@ public final class Constants {
         "July", "August", "September", "October", "November", "December"
     };
 
-    public static final String[] YEARS = {"2024", "2025", "2026", "2027", "2028"};
+    public static final String[] YEARS = {"2015","2016","2017","2018","2019",
+    	    "2020","2021","2022","2023","2024",
+    	    "2025","2026","2027","2028","2029",
+    	    "2030","2031","2032","2033","2034",
+    	    "2035"}; // New
     public static final NumberFormat PESO = NumberFormat.getCurrencyInstance(new Locale("fil", "PH"));
 
     private Constants() {}
